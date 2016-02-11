cf1fe9112d3a1c26addea0888b03d5f4362ea54f
Prateek_Coder <prateekgaurav23@gmail.com>
2016-02-11 14:08:01 +0530

    Complete Day!

--- 11_Find_the_Day.js
+++ 11_Find_the_Day.js
@@ -1,3 +1,25 @@
+/*
+Write a JavaScript function to get the day of a particular date.
+Input Format
+
+Several lines of input containing dates in MM/DD/YYYY format.
+The program should end when it encounters −1−1.
+
+Output Format
+
+Print the day of the week indicated by the date for each line of input on a separate line.
+
+Sample Input:
+
+10/11/2009
+11/10/2010
+-1
+Sample Output:
+
+Sunday
+Wednesday
+*/
+
 function findDay(myDate) {
     // Return day for date myDate(MM/DD/YYYY)
     // Note that myDate contains the date in string format
