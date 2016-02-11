0e5c230c8fcaff0999c7d320a5a2979ec2046443
Prateek_Coder <prateekgaurav23@gmail.com>
2016-02-11 14:07:24 +0530

    Complete Second Largest!

--- 10_Find_The_Second_Largest_Number_In_Array.js
+++ 10_Find_The_Second_Largest_Number_In_Array.js
@@ -1,3 +1,24 @@
+/*
+You are given NN numbers. Store them in an array and find the second largest number.
+
+Input Format
+
+The first line contains NN, the size of array AA.
+The second line contains NN space separated elements of array AA.
+
+Output Format
+
+Output the value of the second largest number in array AA.
+
+Sample Input:
+
+5
+2 3 6 6 5
+Sample Output:
+
+5
+*/
+
 function processData(myArray) {
     var array = myArray;
     var max = Math.max(array[0], array[1]);
