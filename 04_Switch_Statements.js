6208c753e1845961872ff36d0e6ecf3cf5aebcd3
Prateek_Coder <prateekgaurav23@gmail.com>
2016-02-11 14:03:53 +0530

    Complete Switch

--- 04_Switch_Statements.js
+++ 04_Switch_Statements.js
@@ -1,3 +1,19 @@
+/*
+Task
+
+You are given a variable num. Your task is to print:
+
+- ONE, if num is equal to 11.
+- TWO, if num is equal to 22.
+- THREE, if num is equal to 33.
+- FOUR, if num is equal to 44.
+- FIVE, if num is equal to 55.
+- SIX, if num is equal to 66.
+- SEVEN, if num is equal to 77.
+- EIGHT, if num is equal to 88.
+- NINE, if num is equal to 99.
+- PLEASE TRY AGAIN, if num is none of the above.
+*/
 //Do not declare variable num.
 //Write your code below this line.
 switch(num){
