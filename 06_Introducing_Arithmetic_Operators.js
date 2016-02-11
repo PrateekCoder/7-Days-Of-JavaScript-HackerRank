97aa795e123eb6838e35e7ca9bda96c72ca802f1
Prateek_Coder <prateekgaurav23@gmail.com>
2016-02-11 14:04:55 +0530

    Complete Arithmetic Operators

--- 06_Introducing_Arithmetic_Operators.js
+++ 06_Introducing_Arithmetic_Operators.js
@@ -1,3 +1,17 @@
+/*
+Task
+
+You are given two number variables a and b. Both of them have been declared and assigned.
+
+Your task is to :
+
+Create a variable add and assign it with the summation of a and b.
+Create a variable sub and assign it with the subtraction of b from a.
+Create a variable mul and assign it with the multiplication of a and b.
+Create a variable div and assign it with the division of a by b.
+Create a variable inc and assign it with the incremented value of a.
+Create a variable dec and assign it with the decremented value of b.
+*/
 //Do not declare variables `a` and `b`.
 //Write your code below this line.
 
