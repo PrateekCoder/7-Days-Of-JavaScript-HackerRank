1d27f0a6ade33bcdc484dfbc2c2df37989c48b3d
Prateek_Coder <prateekgaurav23@gmail.com>
2016-02-11 14:03:28 +0530

    Complete If-Else

--- 03_If-else_statements.js
+++ 03_If-else_statements.js
@@ -1,3 +1,18 @@
+/*
+Task
+
+You are given a variable marks. Your task is to print:
+
+- AA if marks is greater than 9090.
+- AB if marks is greater than 8080 and less than or equal to 9090.
+- BB if marks is greater than 7070 and less than or equal to 8080.
+- BC if marks is greater than 6060 and less than or equal to 7070.
+- CC if marks is greater than 5050 and less than or equal to 6060.
+- CD if marks is greater than 4040 and less than or equal to 5050.
+- DD if marks is greater than 3030 and less than or equal to 4040.
+- FF if marks is less than or equal to 3030.
+*/
+
 //Do not declare variable marks.
 //Write your code below this line.
 
