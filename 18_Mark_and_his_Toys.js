19e7ec6e7656dc2f69f2aefb49f417822c118d33
Prateek_Coder <prateekgaurav23@gmail.com>
2016-02-14 00:49:35 +0530

    Complete Mark Toys

---
+++ 18_Mark_and_his_Toys.js
@@ -0,0 +1,54 @@
+/*
+Mark and Jane are very happy after having their first kid, and Mark wants to buy some toys for him. There are NN different toys, tagged with their prices, but he has only $K$K. He wants to buy the maximum number of toys for his son.
+
+You are Mark's best friend and have to help him buy as many toys as possible.
+
+Input Format
+
+The first line contains two integers, NN and KK separated by a space.
+The next line contains NN space separated integers indicating the price of each toy.
+
+Output Format
+
+Output an integer that denotes the maximum number of toys Mark can buy with $K$K.
+
+Constraints
+1<=N<=1051<=N<=105
+1<=K<=1091<=K<=109
+1<=price of any toy<=1091<=price of any toy<=109
+A toy can't be bought multiple times.
+
+Sample Input
+
+7 50
+1 12 5 111 200 1000 10
+Sample Output
+
+4
+Explanation
+
+He can buy only 44 toys at the most. These toys have the following prices: 1,12,5,101,12,5,10.
+*/
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
+
+function processData(input) {
+    var rows = input.split('\n');
+    var max = rows[0].split(' ').map(Number)[1];
+    var arr = rows[1].split(' ').map(Number);
+    arr = arr.sort(function(a, b){return a-b});
+    var i = 0;
+    while(max>=0) {
+        max -= arr[i];
+        i++;
+    }
+    console.log(i-1);
+}
